b6e861065058190d328035b058ea2345e9f0ee5e
zdumas1 <zdumas1@emich.edu>
2025-09-30 15:58:00 +0000

    Updating sample run

--- presidio-anonymizer/presidio_anonymizer/sample.py
+++ presidio-anonymizer/presidio_anonymizer/sample.py
@@ -1,7 +1,7 @@
 from presidio_anonymizer import AnonymizerEngine
 from presidio_anonymizer.entities import RecognizerResult, OperatorConfig
 
-def sample_run_anonymizer():
+def sample_run_anonymizer(text: str, start: int, end: int):
     # Initialize the engine
     engine = AnonymizerEngine()
 
@@ -9,13 +9,13 @@ def sample_run_anonymizer():
     # analyzer results (potentially coming from presidio-analyzer) and
     # Operators to get the anonymization output:
     result = engine.anonymize(
-        text=input("text: "),
-        analyzer_results=[RecognizerResult(entity_type="PERSON", start=int(input("start: ")), end=int(input("end: ")), score=0.8)],
+        text=text,
+        analyzer_results=[
+            RecognizerResult(entity_type="PERSON", start=start, end=end, score=0.8)
+        ],
         operators={"PERSON": OperatorConfig("replace", {"new_value": "BIP"})}
     )
-
-    print(result)
-
+    return result
     # input should be:
     # text: My name is Bond.
     # start: 11
@@ -29,4 +29,9 @@ def sample_run_anonymizer():
     # ]
 
 if __name__ == "__main__": 
-    sample_run_anonymizer();
+    text = input("text: ")
+    start = int(input("start: "))
+    end = int(input("end: "))
+
+    result = sample_run_anonymizer(text, start, end)
+    print(result);
